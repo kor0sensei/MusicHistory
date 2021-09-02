949562684a15cfeaa99d38b611a2a7bb18e65b01
kor0sensei <averyolmstead@comcast.net>
2021-09-02 13:25:12 -0500

    got through 11-13

--- MusicHistoryQuerry.sql
+++ MusicHistoryQuerry.sql
@@ -16,7 +16,7 @@
 --#4
 --SELECT DISTINCT
 --ArtistName,
---Name
+--Name GenreName
 --FROM Artist
 --INNER JOIN Album
 --ON Album.ArtistId = Artist.Id
@@ -27,7 +27,7 @@
 --#5
 --SELECT DISTINCT
 --ArtistName,
---Name
+--Name GenreName
 --FROM Artist
 --INNER JOIN Album
 --ON Album.ArtistId = Artist.Id
@@ -38,9 +38,13 @@
 --GenreId = 4
 
 --#6
---SELECT *
+--SELECT
+--ALbum.Title AlbumTitle,
+--Song.Title SongTitle
 --FROM Album
---WHERE AlbumLength = 0
+--LEFT OUTER JOIN Song
+--ON Song.AlbumId = ALbum.Id
+--WHERE Song.Id IS NULL;
 
 --#7
 --INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Lil Uzi Vert', 2016)
@@ -54,13 +58,40 @@
 --INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Baby Are You Home', 230, '4/15/2016', 13, 33, 23)
 
 --#10
-SELECT 
-Album.Title,
-Song.Title,
-ArtistName
-FROM Song
-LEFT JOIN Album
-ON song.AlbumId = Album.Id
-LEFT JOIN Artist
-ON Album.ArtistId = Artist.Id
-WHERE ArtistName = 'Lil Uzi Vert'
+--SELECT 
+--Album.Title AlbumTitle,
+--Song.Title SongTitle,
+--ArtistName
+--FROM Song
+--LEFT JOIN Album
+--ON song.AlbumId = Album.Id
+--LEFT JOIN Artist
+--ON Album.ArtistId = Artist.Id
+--WHERE ArtistName = 'Lil Uzi Vert'
+
+--#11
+--SELECT 
+--Album.Title,
+--COUNT(*) SongCount
+--FROM Song
+--LEFT JOIN Album
+--ON Song.AlbumId = Album.Id
+--GROUP BY Album.Title
+
+--#12
+--SELECT
+--ArtistName,
+--COUNT(*) SongCount
+--FROM Song
+--LEFT JOIN Artist
+--ON Song.ArtistId = Artist.Id
+--GROUP BY ArtistName
+
+--#13
+--SELECT
+--Genre.Name Genre,
+--COUNT(*) SongCount
+--FROM Song
+--LEFT JOIN Genre
+--ON Song.GenreId = Genre.Id
+--GROUP BY Genre.Name
